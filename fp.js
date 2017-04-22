1b142b5cf498cb31d8eaaedfdc8dc31c618ccc81
Paras Sanghavi <paras@codeforamerica.org>
2017-04-22 15:51:48 -0700

    draw rings

--- fp.js
+++ fp.js
@@ -10,11 +10,17 @@ function setup() {
     noFill()
     strokeWeight(3)
     frameRate(15)
+    stroke(255, 204, 0, 25)
 }
 
 function draw() {
-   background(255, 255, 255, 25)
-    ellipse(mouseX, mouseY, 25)
+    background(255, 255, 255, 25)
+
+    var numRings = 10
+
+    for (var i = 0; i < numRings; i++) {
+        drawCircle(50 * i)
+    }
 }
 
 function mousePressed() {
@@ -24,4 +30,19 @@ function mousePressed() {
 
 function randomColor() {
     return _.sample(colors)
+}
+
+function drawCircle(radius) {
+    var numPoints = 20
+
+    var arc = 360 / numPoints
+
+    for (var i = 0; i < numPoints; i++) {
+        var angle = arc * i * Math.PI / 180;
+
+        var xCoordinate = windowWidth / 2 + radius * Math.cos(angle)
+        var yCoordinate = windowHeight / 2 + radius * Math.sin(angle)
+
+        ellipse(xCoordinate, yCoordinate, 25)
+    }
 }
