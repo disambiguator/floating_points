90d35a0c7fbd630a0367bbd8fe3df94315f9894b
Paras Sanghavi <paras@codeforamerica.org>
2017-08-28 22:38:48 -0700

    eslint

--- fp.js
+++ fp.js
@@ -7,89 +7,89 @@ var amplitude
 var direction = 0.1
 
 function setup() {
-    setupVisualizer()
+  setupVisualizer()
 
-    createCanvas(windowWidth, windowHeight)
-    noFill()
-    strokeWeight(3)
-    frameRate(60)
-    stroke(255, 204, 0, 25)
-    background(0, 0, 0)
+  createCanvas(windowWidth, windowHeight)
+  noFill()
+  strokeWeight(3)
+  frameRate(60)
+  stroke(255, 204, 0, 25)
+  background(0, 0, 0)
 
-    initialArc = 0
+  initialArc = 0
 }
 
 function draw() {
-    setRandomStroke()
+  setRandomStroke()
 
-    background(0, 0, 0, 10)
+  background(0, 0, 0, 10)
 
-    // var numRings = 5
+  // var numRings = 5
 
-    var buckets = ['bass', 'lowMid', 'mid', 'highMid', 'treble']
+  var buckets = ['bass', 'lowMid', 'mid', 'highMid', 'treble']
 
-    drawCircle(radiusScale * 0, buckets[0])
-    drawCircle(radiusScale * 1, buckets[1])
-    drawCircle(radiusScale * 2, buckets[2])
-    drawCircle(radiusScale * 3, buckets[3])
-    drawCircle(radiusScale * 4, buckets[4])
-    drawCircle(radiusScale * 5, buckets[3])
-    drawCircle(radiusScale * 6, buckets[2])
-    drawCircle(radiusScale * 7, buckets[1])
-    drawCircle(radiusScale * 8, buckets[0])
+  drawCircle(radiusScale * 0, buckets[0])
+  drawCircle(radiusScale * 1, buckets[1])
+  drawCircle(radiusScale * 2, buckets[2])
+  drawCircle(radiusScale * 3, buckets[3])
+  drawCircle(radiusScale * 4, buckets[4])
+  drawCircle(radiusScale * 5, buckets[3])
+  drawCircle(radiusScale * 6, buckets[2])
+  drawCircle(radiusScale * 7, buckets[1])
+  drawCircle(radiusScale * 8, buckets[0])
 
-    if (Math.abs(offset) > 15)
-        direction = -direction
+  if (Math.abs(offset) > 15)
+    direction = -direction
 
-    offset += direction
+  offset += direction
 
-    initialArc += 0.2
+  initialArc += 0.2
 }
 
 function mousePressed() {
-    stroke(255, 204, 0, 25)
-    ellipse(mouseX, mouseY, 25)
+  stroke(255, 204, 0, 25)
+  ellipse(mouseX, mouseY, 25)
 }
 
 function randomColor() {
-    return _.sample(colors)
+  return _.sample(colors)
 }
 
 function drawCircle(radius, bucket) {
-    var numPoints = radius / 8
+  var numPoints = radius / 8
 
-    fft.analyze()
+  fft.analyze()
 
-    var arc = 360 / numPoints
+  var arc = 360 / numPoints
 
-    for (var i = 0; i < numPoints; i++) {
-        var angle = (arc * i + initialArc) * Math.PI / 180
+  for (var i = 0; i < numPoints; i++) {
+    var angle = (arc * i + initialArc) * Math.PI / 180
 
-        var xCoordinate = windowWidth / 2 + (radius + offset * radius) * Math.cos(angle)
-        var yCoordinate = windowHeight / 2 + (radius + offset * radius) * Math.sin(angle)
+    var xCoordinate = windowWidth / 2 + (radius + offset * radius) * Math.cos(angle)
+    var yCoordinate = windowHeight / 2 + (radius + offset * radius) * Math.sin(angle)
 
-        ellipse(xCoordinate, yCoordinate, Math.pow(fft.getEnergy(bucket), 2)/100)
-    }
+    ellipse(xCoordinate, yCoordinate, Math.pow(fft.getEnergy(bucket), 2) / 100)
+  }
 }
 
 function preload() {
-    mySound = loadSound('/quantic.mp3')
+  mySound = loadSound('/quantic.mp3')
 }
 
 function setRandomStroke() {
-    r = random(127, 255)
-    g = random(127, 255)
-    b = random(127, 255)
+  r = random(127, 255)
+  g = random(127, 255)
+  b = random(127, 255)
 
-    stroke(r, g, b, 50)
+  stroke(r, g, b, 50)
 }
 
 function setupVisualizer() {
-    mySound.setVolume(0.1)
-    mySound.jump(random(0, mySound.duration()))
+  mySound.setVolume(0.1)
+  mySound.jump(random(0, mySound.duration()))
 
-    fft = new p5.FFT()
-    fft.setInput(mySound)
+  fft = new p5.FFT()
+  fft.setInput(mySound)
 
-    amplitude = new p5.Amplitude()
+  amplitude = new p5.Amplitude()
 }
