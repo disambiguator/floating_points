4b145c8039bcbed4c6c99fdbbe6af70fad3664f0
Paras Sanghavi <insomniac18@gmail.com>
2018-12-24 16:48:21 -0500

    add phi for 3d!!

--- src/spiro.js
+++ src/spiro.js
@@ -22,14 +22,17 @@ function addComplexity() {
   positions.push({
     radius: randInt(50, 300),
     arc: randInt(0, 360),
-    speed: randInt(1, 10) * 360 / (randInt(10, 100) + numPoints)
+    phi: randInt(0, 360),
+    speed: randInt(1, 10) * 360 / (randInt(10, 100) + numPoints),
+    phiSpeed: randInt(1, 10) * 360 / (randInt(10, 100) + numPoints)
   })
 }
 
-function getPoint(radius, angle) {
-  const xCoordinate = windowWidth / 2 + radius * Math.cos(angle)
-  const yCoordinate = windowHeight / 2 + radius * Math.sin(angle)
-  return {x: xCoordinate, y: yCoordinate}
+function getPoint(radius, theta, phi) {
+  const xCoordinate = radius * Math.sin(theta) * Math.cos(phi)
+  const yCoordinate = radius * Math.cos(theta) * Math.sin(phi)
+  const zCoordinate = radius * Math.cos(theta)
+  return {x: xCoordinate, y: yCoordinate, z: zCoordinate}
 }
 
 function sum(array, f) {
@@ -59,24 +62,24 @@ function depthGlitch() {
 
 function animate() {
   vertices.forEach(function(v) {
-    const points = positions.map(p => getPoint(p.radius, p.arc))
+    const points = positions.map(p => getPoint(p.radius, p.arc, p.phi))
 
     positions.forEach(function(p) {
       p.arc += p.speed
+      p.phi += p.phiSpeed
     })
 
-    const points_2 = positions.map(p => getPoint(p.radius, p.arc))
+    const points_2 = positions.map(p => getPoint(p.radius, p.arc, p.phi))
 
     const x1 = sum(points, p => p.x) / points.length
     const y1 = sum(points, p => p.y) / points.length
+    const z1 = sum(points, p => p.z) / points.length
     const x2 = sum(points_2, p => p.x) / points.length
     const y2 = sum(points_2, p => p.y) / points.length
+    const z2 = sum(points_2, p => p.z) / points.length
 
-    v.geometry.vertices[0].setComponent(0, x1)
-    v.geometry.vertices[0].setComponent(1, y1)
-
-    v.geometry.vertices[1].setComponent(0, x2)
-    v.geometry.vertices[1].setComponent(1, y2)
+    v.geometry.vertices[0].set(x1, y1, z1)
+    v.geometry.vertices[1].set(x2, y2, z2)
     v.geometry.verticesNeedUpdate = true
   })
 
@@ -87,6 +90,10 @@ function animate() {
   requestAnimationFrame(animate)
 }
 
+function zSlider(element) {
+  camera.position.z = element.value
+}
+
 function runSpiro(bindingElement) {
   _.times(10, addComplexity)
 
@@ -94,13 +101,13 @@ function runSpiro(bindingElement) {
   renderer.setSize(windowWidth, windowHeight)
   bindingElement.appendChild(renderer.domElement)
 
-  camera = new THREE.OrthographicCamera(
-    0, windowWidth,
-    0, windowHeight,
+  camera = new THREE.PerspectiveCamera(
+    45,
+    windowWidth/windowHeight,
     1,
     1000
   )
-  camera.position.set(0, 0, 100)
+  camera.position.set(0, 0, 1000)
   camera.lookAt(0, 0, 0)
 
   scene = new THREE.Scene()
@@ -113,8 +120,8 @@ function runSpiro(bindingElement) {
   const material = new THREE.LineBasicMaterial({color: 0xffffff})
   for (let i = 0; i < renderSpeed; i++) {
     const geometry = new THREE.Geometry()
-    geometry.vertices.push(new THREE.Vector3(0, 0, 0))
-    geometry.vertices.push(new THREE.Vector3(0, 0, 0))
+    geometry.vertices.push(new THREE.Vector3(0, 0, 1))
+    geometry.vertices.push(new THREE.Vector3(0, 0, 1))
     const line = new THREE.Line(geometry, material)
     vertices[i] = line
     scene.add(line)
@@ -125,6 +132,7 @@ function runSpiro(bindingElement) {
 
 (function(window) {
   window.depthGlitch = depthGlitch
+  window.zSlider = zSlider
 })(window)
 
 export default runSpiro
