73dddf62ef601a780bea68e4873054009ff11267
Paras Sanghavi <insomniac18@gmail.com>
2019-01-22 18:55:38 -0800

    remove shades

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import _ from 'lodash'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import runShades from './shades.js'
 import runSpiro from './spiro.js'
 import runMicromovements from './micromovements.js'
-
-const shades = document.getElementById('shades')
-if (shades) {runShades(shades)}
 
 const spiro = document.getElementById('spiro')
 if (spiro) {runSpiro(spiro)}
