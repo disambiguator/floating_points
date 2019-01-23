ff0145adfba6f294319f3b35bf6382dc10d14f88
Paras Sanghavi <insomniac18@gmail.com>
2019-01-22 18:51:54 -0800

    Remove p5 from web packed assets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import _ from 'lodash'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import p5 from 'p5'
 import runShades from './shades.js'
 import runSpiro from './spiro.js'
 import runMicromovements from './micromovements.js'
