b6c1a41304cc7ce049dfae8f5fe1d9d9b3babd00
Paras Sanghavi <insomniac18@gmail.com>
2019-01-26 16:28:01 -0800

    experimenting with next’s

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default () => (
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="pixel_sort.html">Pixelsorting</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="spiro">Spirographs</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="visualizer.html">Visualizer</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"></a>
+        </li>
+    </ul>
+);
