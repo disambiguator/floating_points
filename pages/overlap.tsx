f22545411cccce0a384a3f4cacbe6f244f79b2f5
Paras Sanghavi <insomniac18@gmail.com>
2019-11-24 22:56:55 -0800

    move to functional component

--- pages/overlap.tsx
+++ pages/overlap.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import Scene from '../components/scene'
+import React, { useEffect, useState } from 'react'
+import Scene from '../components/composable_scene'
 import styled from 'styled-components'
 import * as THREE from 'three'
-import { OrbitControls } from 'three-orbitcontrols-ts'
 
 const generateCube = (length) => {
   const geometry = new THREE.BoxGeometry(length, length, length)
@@ -28,58 +27,78 @@ const Container = styled.div`
   background: lightgray;
 `
 
-class CubeZoom extends Scene<{}> {
-  camera: THREE.PerspectiveCamera
-  controls: OrbitControls
+const camera = (width, height) => {
+  const perspectiveCamera = new THREE.PerspectiveCamera(
+    75,
+    width / height,
+    0.1,
+    10000
+  )
+  perspectiveCamera.position.z = 300
+  perspectiveCamera.position.x = 300
+  perspectiveCamera.position.y = 300
 
-  componentDidMount () {
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
+  return perspectiveCamera
+}
 
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      10000
-    )
-    this.camera.position.z = 300
-    this.camera.position.x = 300
-    this.camera.position.y = 300
+const renderer = (width, height) => {
+  const webGLRenderer = new THREE.WebGLRenderer()
+  webGLRenderer.setSize(width, height)
 
-    this.renderer = new THREE.WebGLRenderer()
-    this.renderer.setSize(width, height)
-    this.mount.appendChild(this.renderer.domElement)
-    // Add OrbitControls so that we can pan around with the mouse.
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+  return webGLRenderer
+}
 
-    const increment = 30
-    let previousCube = generateCube(increment)
-    this.scene.add(previousCube)
-    const direction = () => Math.random() > 0.5 ? -1 : -1
-    for (let i = increment * 2; i < 500; i = i + increment) {
-      const newCube = generateCube(i)
-      newCube.position.x = previousCube.position.x + direction() * increment / 2
-      newCube.position.y = previousCube.position.y + direction() * increment / 2
-      newCube.position.z = previousCube.position.z + direction() * increment / 2
-      this.scene.add(newCube)
-      previousCube = newCube
+const cubes = () => {
+  const shapes = []
+  const increment = 30
+  let previousCube = generateCube(increment)
+  shapes.push(previousCube)
+  const direction = () => Math.random() > 0.5 ? -1 : -1
+  for (let i = increment * 2; i < 500; i = i + increment) {
+    const newCube = generateCube(i)
+    newCube.position.x = previousCube.position.x + direction() * increment / 2
+    newCube.position.y = previousCube.position.y + direction() * increment / 2
+    newCube.position.z = previousCube.position.z + direction() * increment / 2
+    shapes.push(newCube)
+    previousCube = newCube
+  }
+
+  return shapes
+}
+
+const CubeZoom = (props) => {
+  const { width, height } = props
+  const sceneCamera = camera(width, height)
+  const renderScene = () => {
+    // sceneCamera.translateX(-1)
+    sceneCamera.translateX(-0.5)
+    sceneCamera.translateZ(-0.5)
+  }
+
+  return (
+    <Scene
+      camera={sceneCamera}
+      shapes={cubes()}
+      renderer={renderer(width, height)}
+      renderScene={renderScene}
+      orbitControls
+    />
+  )
+}
+
+const Page = () => {
+  const [dimensions, setDimensions] = useState(null)
+  const myRef: React.RefObject<HTMLDivElement> = React.createRef()
+
+  useEffect(() => {
+    if (dimensions == null) {
+      setDimensions({ width: myRef.current.clientWidth, height: myRef.current.clientHeight })
     }
+  })
 
-    this.start()
-  }
-
-  renderScene = () => {
-    // this.camera.translateX(-1)
-    this.camera.translateX(-0.5)
-    this.camera.translateZ(-0.5)
-    this.controls.update()
-    this.renderer.render(this.scene, this.camera)
-  }
-
-  render () {
-    return (
-      <Container>
-        <style global jsx>{`
+  return (
+    <Container>
+      <style global jsx>{`
       html,
       body,
       body > div:first-child,
@@ -89,14 +108,14 @@ class CubeZoom extends Scene<{}> {
         height: 100%;
       }
     `}</style>
-
-        <div
-          style={{ width: '1200px', height: '1200px' }}
-          ref={mount => { this.mount = mount }}
-        />
-      </Container>
-    )
-  }
+      <div
+        style={{ width: '1200px', height: '1200px' }}
+        ref={myRef}
+      >
+        {dimensions ? <CubeZoom height={dimensions.height} width={dimensions.width} /> : null}
+      </div>
+    </Container>
+  )
 }
 
-export default CubeZoom
+export default Page
