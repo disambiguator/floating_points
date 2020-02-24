21dbcecfa874b87614b0c0ad4ac4dd49e47a5cd9
Paras Sanghavi <insomniac18@gmail.com>
2020-02-23 17:51:35 -0800

    upgrade dependencies and add back orbit control lib

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three-orbitcontrols-ts';
 
 type Props = {
   renderer: THREE.WebGLRenderer;
