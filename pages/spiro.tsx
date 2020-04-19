35edba254b92328b76e7488ecef2c73994c499c3
Paras Sanghavi <insomniac18@gmail.com>
2020-04-19 16:54:19 -0700

    move fiber to spiro

--- pages/spiro.tsx
+++ pages/spiro.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 
 export default () => {
-  // const Spiro = dynamic(() => import('../components/spiro-fiber'), {
-  const Spiro = dynamic(() => import('../components/spiro-fiber'), {
+  const Spiro = dynamic(() => import('../components/spiro'), {
     ssr: false,
   });
   return <Spiro />;
