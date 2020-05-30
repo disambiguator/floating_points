8fd56f081eaab8edaa1eefaf2aa26443ad6497b8
Paras Sanghavi <insomniac18@gmail.com>
2020-05-30 13:14:06 -0700

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import 'react-dat-gui/dist/index.css';
+import Router from 'next/router';
+import { useAnalytics } from '../hooks/useAnalytics';
+
+// https://johnkueh.com/articles/adding-a-use-analytics-hook
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { init, trackPageViewed } = useAnalytics();
+  useEffect(() => {
+    init('UA-168065998-1');
+    trackPageViewed();
+    Router.events.on('routeChangeComplete', () => {
+      trackPageViewed();
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
