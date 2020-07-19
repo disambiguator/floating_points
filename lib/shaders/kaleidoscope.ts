5b287ac51a4408f25d86f6e9a734d3576633fc17
Paras Sanghavi <insomniac18@gmail.com>
2020-07-18 21:12:36 -0700

    fix broken pages

--- lib/shaders/kaleidoscope.ts
+++ lib/shaders/kaleidoscope.ts
@@ -29,9 +29,8 @@ export const KaleidoscopeShader = {
 
     const float time = 0.0;
 
-    float KA = PI / numSides;
-
     vec2 smallKoleidoscope(vec2 uv) {
+        float KA = PI / numSides;
         // get the angle in radians of the current coords relative to origin (i.e. center of screen)
         float angle = atan (uv.y, uv.x);
         // repeat image over evenly divided rotations around the center
@@ -41,12 +40,12 @@ export const KaleidoscopeShader = {
         // rotate image over time
         // angle += 0.1*time;
         // get the distance of the coords from the uv origin (i.e. center of the screen)
-        float d = length(uv); 
+        float d = length(uv);
         // map the calculated angle to the uv coordinate system at the given distance
         return d * vec2(cos(angle), sin(angle));
     }
 
-    vec2 reflect(vec2 i) {
+    vec2 reflectImage(vec2 i) {
       return mod(i,1.)*-(step(1.,i)*2.-1.) + step(1.0,i);
     }
 
@@ -54,7 +53,7 @@ export const KaleidoscopeShader = {
       vec2 position = vUv * 2. - 1.;
       position.x *= aspect;
 
-      position = reflect(smallKoleidoscope(position));
+      position = reflectImage(smallKoleidoscope(position));
       gl_FragColor = texture2D(tDiffuse, position);
     }
   `,
