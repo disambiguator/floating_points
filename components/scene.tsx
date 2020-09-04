df5a4414a62105eb67ad5b8344ff35547760c179
Paras Sanghavi <insomniac18@gmail.com>
2020-09-03 23:19:32 -0700

    update

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,18 +1,7 @@
 import React, { useRef } from 'react';
-import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { Pass } from 'three/examples/jsm/postprocessing/Pass';
 import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
 import { ContainerProps } from 'react-three-fiber/targets/shared/web/ResizeContainer';
-
-type Props = {
-  renderer: THREE.WebGLRenderer;
-  renderScene: () => void;
-  orbitControls?: boolean;
-  camera: THREE.Camera;
-  shapes: Array<THREE.Mesh | THREE.Line>;
-  effects?: Array<Pass>;
-};
 
 // Make OrbitControls known as <orbitControls />
 extend({ OrbitControls });
