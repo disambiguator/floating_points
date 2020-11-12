63b4057eef1b11bdfb31c420249bea47ae583ecb
Paras Sanghavi <insomniac18@gmail.com>
2020-11-11 20:28:35 -0800

    rename to void

--- pages/void.tsx
+++ pages/void.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 
 export default function PerlinField() {
-  const Spiro = dynamic(() => import('../components/perlinfield copy'), {
+  const Spiro = dynamic(() => import('../components/void'), {
     ssr: false,
   });
 
