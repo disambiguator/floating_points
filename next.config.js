121daa6053414c181c6a28f69f764e7ce75b2053
Paras Sanghavi <insomniac18@gmail.com>
2020-11-21 20:04:56 -0800

    transpile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPlugins = require('next-compose-plugins');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')([
+  'three',
+  // 'postprocessing',
+  // 'react-three-gui',
+  // 'drei',
+  'react-dat-gui',
+]);
+
+module.exports = withPlugins([[withTM, {}]], {
   target: 'serverless',
   typescript: {
     ignoreDevErrors: true,
   },
-};
+});
