f51a88100d14e3e65b17d4410b2c7e47d268c31d
Paras Sanghavi <insomniac18@gmail.com>
2021-01-01 13:02:57 -0800

    drei orbitcontrols

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,25 +1,14 @@
-import React, { useRef } from 'react';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { extend, useThree, useFrame } from 'react-three-fiber';
+import React from 'react';
 import { ContainerProps } from 'react-three-fiber/targets/shared/web/ResizeContainer';
 import { Controls } from 'react-three-gui';
+import { OrbitControls } from '@react-three/drei/OrbitControls';
 
-// Make OrbitControls known as <orbitControls />
-extend({ OrbitControls });
-
-function SControls() {
-  const ref = useRef<OrbitControls>();
-  const { camera, gl } = useThree();
-  useFrame(() => ref.current!.update());
-  // @ts-ignore
-  return <orbitControls ref={ref} args={[camera, gl.domElement]} />;
-}
 export const FiberScene = (props: ContainerProps & { controls?: boolean }) => {
   const { controls, ...rest } = props;
   return (
     <Controls.Provider>
       <Controls.Canvas {...rest}>
-        {controls && <SControls />}
+        {controls && <OrbitControls />}
         {props.children}
       </Controls.Canvas>
       <Controls />
