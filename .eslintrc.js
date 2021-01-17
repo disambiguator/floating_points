ca8a0286115f8da38ba8dfe5e5f7ee203c447a34
Paras Sanghavi <insomniac18@gmail.com>
2021-01-16 22:43:54 -0800

    add a bunch of config from react-three-next

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'react-app',
+    'prettier/react',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
@@ -20,6 +23,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     'prefer-destructuring': 2,
     'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-unused-vars': [
       // Allow unused args/vars to be marked with an underscore
       'error',
--- next.config.js
+++ next.config.js
@@ -1,17 +1,87 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPlugins = require('next-compose-plugins');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require('next-transpile-modules')([
-  'three',
-  // 'postprocessing',
-  'react-three-gui',
-  '@react-three/drei',
-  'react-dat-gui',
-]);
+const plugins = require('next-compose-plugins');
 
-module.exports = withPlugins([[withTM, {}]], {
+// custom transpile module
+const debug = false;
+let start = null;
+let i = 0;
+
+const match = (path) => {
+  if (!path.includes('three/examples/jsm')) return false;
+  if (debug) {
+    // should be around 3/4 seconds the first time and then 200ms after using Webpack 5 built-in loader cache
+    let end = start ? new Date() - start : 0;
+    console.log(
+      `\x1b[37m`,
+      `🚄 ${end}ms - The transpilation ${path} in process`,
+    );
+    if (i === 1) {
+      start = new Date();
+    }
+    i++;
+  }
+  return true;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')(
+  ['three', 'react-three-gui', '@react-three/drei', 'react-dat-gui'], // '@react-three/postprocessing'
+  { debug, unstable_webpack5: false, match }, // symlink-caused loops which cause memory to get bloated exponentially.
+);
+
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push(
+      { test: /react-spring/, sideEffects: true }, // prevent vercel to crash when deploy
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader', 'glslify-loader'],
+      },
+    );
+    config.watchOptions = {
+      ignored: ['**/.git/**', '**/.next/**', '**node_modules/**'],
+    };
+    return config;
+  },
   target: 'serverless',
   typescript: {
     ignoreDevErrors: true,
   },
-});
+};
+
+module.exports = plugins(
+  [
+    withTM(nextConfig),
+    [
+      {
+        workboxOpts: {
+          swDest: process.env.NEXT_EXPORT
+            ? 'service-worker.js'
+            : 'static/service-worker.js',
+          runtimeCaching: [
+            {
+              urlPattern: /^https?.*/,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'offlineCache',
+                expiration: {
+                  maxEntries: 200,
+                },
+              },
+            },
+          ],
+        },
+        async rewrites() {
+          return [
+            {
+              source: '/service-worker.js',
+              destination: '/_next/static/service-worker.js',
+            },
+          ];
+        },
+      },
+    ],
+  ],
+  nextConfig,
+);
