c884afbf4a3f1ade0a61bc950017f04a773261f8
Paras Sanghavi <insomniac18@gmail.com>
2021-02-27 16:50:39 -0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:react-hooks/recommended',
     'react-app',
-    'prettier/react',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
