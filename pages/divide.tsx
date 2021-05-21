cd2f379fa446896d583a247f61d58cb5d6ccc239
Paras Sanghavi <insomniac18@gmail.com>
2021-05-21 15:39:15 -0700

    get rid of weird ssr thing

--- pages/divide.tsx
+++ pages/divide.tsx
@@ -1,8 +1,81 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Page from '../components/page';
 
-export default function Divide() {
-  const Spiro = dynamic(() => import('../components/divide'), { ssr: false });
+const translateDistance = 1;
+const width = 800;
+const height = 800;
 
-  return <Spiro />;
+const Divide = () => {
+  let topX = 0;
+  let bottomX = 0;
+  let timer = 0;
+
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const mount = ref.current!;
+    const ctx = mount.getContext('2d')!;
+    ctx.lineWidth = 5;
+  });
+
+  const animate = () => {
+    const mount = ref.current!;
+    const ctx = mount.getContext('2d')!;
+
+    if (timer % 5 === 0) {
+      topX = Math.random() * 800;
+      bottomX = Math.random() * 800;
+
+      ctx.beginPath();
+      ctx.strokeStyle = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+      ctx.moveTo(topX, 0);
+      ctx.lineTo(bottomX, 800);
+      ctx.stroke();
+    }
+
+    ctx.save();
+
+    const rightRegion = new Path2D();
+    rightRegion.moveTo(topX, 0);
+    rightRegion.lineTo(bottomX, 800);
+    rightRegion.lineTo(800, 800);
+    rightRegion.lineTo(800, 0);
+    rightRegion.lineTo(topX, 0);
+    ctx.clip(rightRegion);
+    ctx.translate(translateDistance, 0);
+    ctx.drawImage(mount, translateDistance, 0);
+    ctx.restore();
+
+    ctx.save();
+    const leftRegion = new Path2D();
+    leftRegion.moveTo(topX, 0);
+    leftRegion.lineTo(bottomX, 800);
+    leftRegion.lineTo(0, 800);
+    leftRegion.lineTo(0, 0);
+    leftRegion.lineTo(topX, 0);
+    ctx.clip(leftRegion);
+    ctx.translate(-translateDistance, 0);
+    ctx.drawImage(mount, -translateDistance, 0);
+    ctx.restore();
+
+    timer++;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(animate, 20);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  return <canvas width={width} height={height} ref={ref} />;
+};
+
+export default function DividePage() {
+  return (
+    <Page>
+      <Divide />
+    </Page>
+  );
 }
