82fe4aa66e4ead89937b9b4e7b8d7fc643d276c5
Paras Sanghavi <insomniac18@gmail.com>
2021-06-30 11:26:59 -0700

    delete draft sktch

--- pages/sin.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
-
-export default function Sin() {
-  const Spiro = dynamic(() => import('../components/sin'), { ssr: false });
-
-  return <Spiro />;
-}
