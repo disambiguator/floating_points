d7bce25b906276289356331f24c91ade3290bc93
Paras Sanghavi <insomniac18@gmail.com>
2021-06-30 16:58:37 -0700

    fix asset url

--- lib/assetUrl.ts
+++ lib/assetUrl.ts
@@ -1,6 +1,6 @@
 const assetUrl = (suffix: string) =>
   process.env.NODE_ENV === 'development'
     ? suffix
-    : `https://floating-points.s3.us-east-2.amazonaws.com/${suffix}`;
+    : `https://floating-points.s3.us-east-2.amazonaws.com${suffix}`;
 
 export default assetUrl;
