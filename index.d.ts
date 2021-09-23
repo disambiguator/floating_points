e99e97b5ed89a7ee34364e7ce01b5b531ab57e3d
Paras Sanghavi <insomniac18@gmail.com>
2021-09-22 21:30:55 -0400

    update react p5 wrapper and remove custom types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,3 @@
-declare module 'react-p5-wrapper' {
-  import { P5WrapperComponent as ReactP5Wrapper } from './lib/types';
-  export { ReactP5Wrapper };
-}
 declare module 'glslify' {
   import { Glslify } from './lib/types';
   export default Glslify;
--- lib/types.ts
+++ lib/types.ts
@@ -1,14 +1,6 @@
-import * as p5 from 'p5';
-import { ComponentType } from 'react';
-
 export interface Dimensions {
   width: number;
   height: number;
 }
-
-interface P5WrapperProps {
-  sketch: (p: p5) => void;
-}
-export type P5WrapperComponent = ComponentType<P5WrapperProps>;
 
 export type Glslify = unknown;
