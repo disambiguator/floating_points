f5dc379075d2b402f55639ba52f95be92fc08393
Paras Sanghavi <insomniac18@gmail.com>
2022-01-17 13:08:06 -0800

    WIP

--- pages/genuary/15.tsx
+++ pages/genuary/15.tsx
@@ -1,160 +1,76 @@
-import { Box } from '@react-three/drei';
-import { useFrame, useThree } from '@react-three/fiber';
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import { useFrame } from '@react-three/fiber';
+import glsl from 'glslify';
+import React, { useRef } from 'react';
 import * as THREE from 'three';
+import Page from 'components/page';
 import { FiberScene } from 'components/scene';
-import Page from '../../components/page';
 
-const Electron = React.memo(function Electron(props: {
-  position: THREE.Vector3;
-  direction: THREE.Vector3;
-  remove: () => void;
-}) {
-  const ref = useRef<THREE.Mesh>();
-  const scene = useThree((t) => t.scene);
+function buildTwistMaterial() {
+  const material = new THREE.MeshNormalMaterial();
+  material.onBeforeCompile = function (shader) {
+    shader.uniforms.time = { value: 0 };
 
-  const [direction, setDirection] = useState(props.direction);
+    shader.vertexShader =
+      glsl`
+    #pragma glslify: snoise2 = require(glsl-noise/simplex/2d)
+    uniform float time;
 
-  const scaledDirection = useMemo(
-    () => direction.clone().divideScalar(10),
-    [direction],
-  );
-
-  const raycaster = useMemo(
-    () => new THREE.Raycaster(undefined, undefined, 0, 1),
-    [],
-  );
-  useFrame(() => {
-    const mesh = ref.current!;
-    raycaster.set(mesh.position, direction);
-    const intersects = raycaster.intersectObjects(scene.children);
-    if (intersects.length > 0) {
-      setDirection((direction) => {
-        const vector = direction.clone();
-        const axis = new THREE.Vector3(0, 1, 0);
-        const angle = Math.PI / 2;
-        vector.applyAxisAngle(axis, angle);
-        return vector;
-      });
-    } else {
-      mesh.position.add(scaledDirection);
-      if (mesh.position.length() > size) {
-        props.remove();
-      }
+    float noise(vec2 pos) {
+        return snoise2(pos.xy/20.) * 10.;
     }
-  });
+    ` + shader.vertexShader;
+    shader.vertexShader = shader.vertexShader.replace(
+      '#include <begin_vertex>',
+      glsl`
+        vec3 transformed = position;
+        transformed.z = noise(position.xy);
+        vNormal = normalize(
+            vec3(
+                noise(vec2(position.x + 1., position.y)) - noise(position.xy),
+                // noise(vec2(position.x, position.y + 1.)) - noise(position.xy),
+                1.,
+                0.
+                )
+            );
+      `,
+    );
 
-  return (
-    <Box ref={ref} position={props.position}>
-      <meshStandardMaterial color="pink" />
-    </Box>
-  );
-});
-const size = 25;
+    material.userData.shader = shader;
+  };
 
-const randomPosition = (): {
-  position: THREE.Vector3;
-  direction: THREE.Vector3;
-} => {
-  const position = new THREE.Vector3();
-  const direction = new THREE.Vector3();
-  const x = Math.floor(Math.random() * 4);
-  if (x === 0) {
-    position.set(Math.random() * size - size / 2, 0, -size / 2);
-    direction.set(0, 0, 1);
-  } else if (x === 1) {
-    position.set(Math.random() * size - size / 2, 0, size / 2);
-    direction.set(0, 0, -1);
-  } else if (x === 2) {
-    position.set(-size / 2, 0, Math.random() * size - size / 2);
-    direction.set(1, 0, 0);
-  } else {
-    position.set(size / 2, 0, Math.random() * size - size / 2);
-    direction.set(-1, 0, 0);
-  }
-  return { position, direction };
-};
+  // Make sure WebGLRenderer doesnt reuse a single program
 
-const Eclipse = React.memo(function Shader() {
-  const [electrons, setElectrons] = useState<
-    {
-      key: string;
-      remove: () => void;
-      position: THREE.Vector3;
-      direction: THREE.Vector3;
-    }[]
-  >([]);
+  material.customProgramCacheKey = function () {
+    return 'dsjdhas';
+  };
 
-  const removeElectron = useCallback(
-    (key: string) => () => {
-      setElectrons((electrons) => electrons.filter((e) => e.key !== key));
-    },
-    [],
-  );
+  return material;
+}
 
-  const newElectron = useCallback(() => {
-    const key = String(Math.random());
-    return { key, remove: removeElectron(key), ...randomPosition() };
-  }, [removeElectron]);
-
-  useEffect(() => {
-    const initElectrons = Array(200).fill(0).map(newElectron);
-    setElectrons(initElectrons);
-  }, [newElectron, removeElectron]);
-
-  // const i = useRef(0);
+const Scene = React.memo(function Scene() {
+  const ref = useRef<THREE.Group>();
+  // const { depth } = useControls({ depth: { value: 30, min: 0, max: 500 } });
   useFrame(() => {
-    // i.current++;
-    // if (i.current % 10 !== 0) return;
-
-    if (electrons.length < 200) {
-      setElectrons((electrons) => [...electrons, newElectron()]);
-    }
+    ref.current!.rotation.z += 1;
   });
-
   return (
     <>
-      {/* <Plane
-        args={[size * 2, size * 2]}
-        position={[0, -10, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-      >
-        <meshBasicMaterial color="blue" />
-      </Plane> */}
-      {electrons.map(({ remove, key, position, direction }) => {
-        return (
-          <Electron
-            key={key}
-            remove={remove}
-            position={position}
-            direction={direction}
-          />
-        );
-      })}
-      {/* <Electron
-        position={new THREE.Vector3(-size / 2, 0, 0)}
-        direction={new THREE.Vector3(1, 0, 0)}
-      />
-      <Electron
-        position={new THREE.Vector3(size / 2, 0, 0)}
-        direction={new THREE.Vector3(-1, 0, 0)}
-      /> */}
-      <pointLight position={[-size / 2, 10, size / 2]} />
+      <mesh rotation={[-Math.PI / 2, 0, 0]} material={buildTwistMaterial()}>
+        <planeGeometry args={[100, 100, 100, 100]} />
+        {/* <shaderMaterial ref={ref} args={[Shader]} /> */}
+      </mesh>
+      <group ref={ref}>
+        <pointLight position={[0, 100, 0]} />
+      </group>
     </>
   );
 });
 
-export default function ShaderPage() {
+export default function Fifteen() {
   return (
     <Page>
-      <FiberScene controls camera={{ position: [-15, 30, 15] }}>
-        <Eclipse />
+      <FiberScene controls camera={{ position: [0, 20, 1000] }}>
+        <Scene />
       </FiberScene>
     </Page>
   );
