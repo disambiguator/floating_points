a0e1b67c9c15c3e563de3d51212f349ffd8d6533
Paras Sanghavi <paras@disambiguo.us>
2022-10-28 15:10:24 -0700

    Remove plugins

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const plugins = require('next-compose-plugins');
-
-const nextConfig = {
+module.exports = {
   webpack(config) {
-    config.module.rules.push(
-      { test: /react-spring/, sideEffects: true }, // prevent vercel to crash when deploy
-      {
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ['raw-loader', 'glslify-loader'],
-      },
-    );
-    config.watchOptions = {
-      ignored: ['**/.git/**', '**/.next/**', '**node_modules/**'],
-    };
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
     return config;
   },
   typescript: {
@@ -22,39 +13,3 @@ const nextConfig = {
   // Remove this once I figure out hydra-ts nonsense
   experimental: { esmExternals: 'loose' },
 };
-
-module.exports = plugins(
-  [
-    nextConfig,
-    [
-      {
-        workboxOpts: {
-          swDest: process.env.NEXT_EXPORT
-            ? 'service-worker.js'
-            : 'static/service-worker.js',
-          runtimeCaching: [
-            {
-              urlPattern: /^https?.*/,
-              handler: 'NetworkFirst',
-              options: {
-                cacheName: 'offlineCache',
-                expiration: {
-                  maxEntries: 200,
-                },
-              },
-            },
-          ],
-        },
-        async rewrites() {
-          return [
-            {
-              source: '/service-worker.js',
-              destination: '/_next/static/service-worker.js',
-            },
-          ];
-        },
-      },
-    ],
-  ],
-  nextConfig,
-);
