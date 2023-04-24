f389f03f6c1df24350b6fdb65c84bf86cbf5c348
Paras Sanghavi <paras@disambiguo.us>
2023-04-23 22:43:33 -0700

    Debug query param

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,14 +1,22 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas, type Props } from '@react-three/fiber';
-import React from 'react';
+import { useRouter } from 'next/router';
+import { Perf } from 'r3f-perf';
+import React, { useMemo } from 'react';
 
 export const FiberScene = ({
   controls,
   children,
   ...rest
-}: Props & { controls?: boolean }) => (
-  <Canvas {...rest}>
-    {controls && <OrbitControls />}
-    {children}
-  </Canvas>
-);
+}: Props & { controls?: boolean }) => {
+  const router = useRouter();
+  const debug = useMemo(() => !!router.query.debug, [router]);
+
+  return (
+    <Canvas {...rest}>
+      {controls && <OrbitControls />}
+      {debug && <Perf />}
+      {children}
+    </Canvas>
+  );
+};
