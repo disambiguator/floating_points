6ab1e3b88fb8d9ca28bc7011625544533d39b491
Paras Sanghavi <paras@disambiguo.us>
2023-10-24 20:13:06 -0700

    Fix relative model sizing

--- scenes/skull.tsx
+++ scenes/skull.tsx
@@ -1,5 +1,12 @@
 import { useFrame, useLoader } from '@react-three/fiber';
-import React, { useCallback, useMemo, useRef, useState } from 'react';
+import React, {
+  ForwardedRef,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import {
   Color,
   Group,
@@ -21,7 +28,7 @@ import { Orbits } from './halloween';
 //   new Color(56 / 255, 252 / 255, 159 / 255),
 // ];
 
-const lights = new Array(10).fill(undefined).map(() => {
+const lights = new Array(2).fill(undefined).map(() => {
   return {
     radius: 4,
     theta: 2 * Math.PI * Math.random(),
@@ -38,33 +45,41 @@ const lights = new Array(10).fill(undefined).map(() => {
 useLoader.preload(OBJLoader, assetUrl('/pumpkin.obj'));
 useLoader.preload(OBJLoader, assetUrl('/skull.obj'));
 
-function Skull() {
-  const ref = useRef<Mesh>(null);
-  const pumpkin = useLoader(OBJLoader, assetUrl('/pumpkin.obj')) as Object3D;
+const Skull = forwardRef(function Skull(_, ref: ForwardedRef<Object3D>) {
   const skull = useLoader(OBJLoader, assetUrl('/skull.obj')) as Object3D;
-  const objects = useMemo(() => [pumpkin, skull], [pumpkin, skull]);
 
-  const [obj, setObj] = useState(skull);
+  return <primitive ref={ref} object={skull} />;
+});
+
+const Pumpkin = forwardRef(function Pumpkin(_, ref: ForwardedRef<Object3D>) {
+  const pumpkin = useLoader(OBJLoader, assetUrl('/pumpkin.obj')) as Object3D;
+
+  return <primitive scale={4} ref={ref} object={pumpkin} />;
+});
+
+const objects = [Skull, Pumpkin];
+
+function Model() {
+  const ref = useRef<Mesh>(null);
+  const [Obj, setObj] = useState<(typeof objects)[0]>(Skull);
   const nextObject = useCallback(() => {
-    setObj((o) => {
-      return objects[(objects.indexOf(o) + 1) % objects.length];
-    });
-  }, [setObj, objects]);
-
-  const o = useMemo(() => obj.clone(), [obj]);
-  const meshes = useMemo(() => {
+    setObj(
+      (o: typeof Skull) => objects[(objects.indexOf(o) + 1) % objects.length],
+    );
+  }, [setObj]);
+  const meshes = useRef<Mesh<any, MeshStandardMaterial>[]>([]);
+  useEffect(() => {
     const m: Mesh<any, MeshStandardMaterial>[] = [];
-    o.traverse((child) => {
+    ref.current!.traverse((child) => {
       if (child instanceof Mesh) {
         const c = child as Mesh<any, MeshStandardMaterial>;
         m.push(c);
         c.material = new MeshStandardMaterial();
-        // c.material.roughness = 0;
         c.material.clippingPlanes = [new Plane(new Vector3(0, 0, -1), 100)];
       }
     });
-    return m;
-  }, [o]);
+    meshes.current = m;
+  }, [Obj]);
 
   const prevConstant = useRef<number>(0);
   const prevDir = useRef<'up' | 'down'>('up');
@@ -82,14 +97,14 @@ function Skull() {
     prevConstant.current = constant;
     prevDir.current = dir;
 
-    meshes.forEach((m) => {
+    meshes.current.forEach((m) => {
       m.material.clippingPlanes.forEach((plane) => {
         plane.constant = constant;
       });
     });
   });
 
-  return <primitive ref={ref} object={o} />;
+  return <Obj ref={ref} />;
 }
 
 const Halloween = React.memo(function Dusen() {
@@ -107,7 +122,7 @@ const Halloween = React.memo(function Dusen() {
     <>
       <group ref={groupRef}>
         <React.Suspense fallback={null}>
-          <Skull />
+          <Model />
         </React.Suspense>
       </group>
       {lights.map((s, i) => (
