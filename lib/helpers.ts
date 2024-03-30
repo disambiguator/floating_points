2a69d762e12eb1d0ae8a892e66d0a358899b4616
Paras Sanghavi <paras@disambiguo.us>
2024-03-30 15:01:00 -0700

    fix rand helpers

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,18 +2,13 @@ import { Color } from 'three';
 
 // min inclusive, max exclusive
 export const rand = (a: number, b?: number) => {
-  const min = b != null ? a : 0;
-  const max = b ?? a;
-
+  const [min, max] = b != null ? [a, b] : [0, a];
   return min + Math.random() * (max - min);
 };
 
 // min inclusive, max exclusive
 export const randInt = (a: number, b?: number) => {
-  const min = b != null ? a : 0;
-  const max = b ?? a;
-
-  return min + Math.floor(Math.random()) * (max - min);
+  return Math.floor(rand(a, b));
 };
 
 export function scaleExponential(
